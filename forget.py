73380b5fb4ef0f0653a6739f83c36f3ed63ca681
molereddy <m.anmolreddy@gmail.com>
2024-12-14 04:16:39 +0000

    revert forget to cleanup v1

--- forget.py
+++ forget.py
@@ -1,6 +1,6 @@
-from data_module import TextForgetDatasetQA, TextForgetDatasetSubFullQA, TextForgetDatasetIDKFullQA
-from data_module import unlearn_collator, unlearn_collator_sub
+from data_module import TextForgetDatasetQA, TextForgetDatasetIDKFullQA, TextForgetDatasetSubFullQA
 from dataloader import CustomTrainerForgetting
+from data_module import unlearn_collator, unlearn_collator_sub
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, set_seed
 import hydra
@@ -8,12 +8,8 @@ import transformers
 import pandas as pd
 import os, sys, copy, shutil, json
 from pathlib import Path
-# from gib_eval import add_gibberish_evals
 from utils import get_model_identifiers_from_yaml, get_model_utility_v2, rearrange_cols
-# from agg_results.plot_traj import plot_trajectory
-# from agg_results.plot_hist import plot_histograms
 from omegaconf import OmegaConf
-
 
 HF_HOME = os.getenv('HF_HOME', '~/.cache/huggingface')
 
@@ -26,11 +22,15 @@ def main(cfg):
     model_cfg = get_model_identifiers_from_yaml(cfg.model_family)
     model_id = model_cfg["hf_key"]
     
+    
     forget_percentage = int(cfg.split[6:]) # 01, 05 or 10
     cfg.eval.retain_result = os.path.join(model_cfg['retain_evals_path'].format(split=100-forget_percentage), 
                                           'eval_results/ds_size300/eval_log_aggregated.json')
     
     set_seed(cfg.seed)
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+    
     print("Base cfg before prep is", cfg)
     # setup paths
     cfg.model_path = model_cfg["results_path"]
@@ -48,68 +48,54 @@ def main(cfg):
     else:
         assert cfg.model_path in cfg.save_dir
         print("Save path is given to forget.py directly")
+    
     shutil.rmtree(cfg.save_dir, ignore_errors=True)
     Path(cfg.save_dir).mkdir(parents=True, exist_ok=True)
     print("Saving experiment results to: ", os.path.abspath(cfg.save_dir))
     
-    # save cfg
+    shutil.rmtree(cfg.save_dir, ignore_errors=True)
+    Path(cfg.save_dir).mkdir(parents=True, exist_ok=True)
+    
     with open(f"{cfg.save_dir}/config.yaml", "w") as file:
         OmegaConf.save(cfg, file)
 
-    # setup model, tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    tokenizer.pad_token = tokenizer.eos_token
-    
-    model_kwargs = {
-        'attn_implementation': 'flash_attention_2',
-        'torch_dtype': torch.bfloat16,
-        'trust_remote_code': True,
-        'cache_dir': HF_HOME
-    }
-    model = AutoModelForCausalLM.from_pretrained(model_cfg["ft_model_path"], **model_kwargs)
-    # Hot fix for https://discuss.huggingface.co/t/help-with-llama-2-finetuning-setup/50035
-    model.generation_config.do_sample = True
-    if model_cfg["gradient_checkpointing"] == "true":
-        model.gradient_checkpointing_enable()
-    model.config.use_cache = False
+    print("Saving to: ", os.path.abspath(cfg.save_dir))
 
-    max_length = 500
+
+    if os.path.exists(cfg.save_dir):
+        # print(f"Method directory already exists:{os.path.abspath(cfg.save_dir)}")
+        if not cfg.overwrite_dir:
+            exit()
+    if os.path.exists(os.path.join(cfg.save_dir, 'trajectory.png')):
+        print("Results already exist in directory, skipping experiment")
+        sys.exit()
+
+    max_length = 256
+    collator=unlearn_collator
+    
     intial_ckpt_ref_needed = (cfg.retain_type == 'KL')
     if 'npo' in cfg.forget_loss or 'ppo' in cfg.forget_loss or 'dpo' in cfg.forget_loss or 'KL' in cfg.forget_loss:
-        intial_ckpt_ref_needed = True
-        collator = unlearn_collator
+        intial_ckpt_ref_needed=True
     if cfg.forget_loss == "idkdpo": # the idk dpo that the paper found unstable
-        torch_format_dataset = TextForgetDatasetIDKFullQA(data_path=cfg.data_path, tokenizer=tokenizer, 
-                                                          model_family=cfg.model_family, max_length=max_length, 
-                                                          split=cfg.split)
-        collator = unlearn_collator_sub
+        torch_format_dataset = TextForgetDatasetIDKFullQA(data_path=cfg.data_path, tokenizer=tokenizer, model_family=cfg.model_family, max_length=max_length, split=cfg.split)
+        collator=unlearn_collator_sub
         intial_ckpt_ref_needed = True
-    elif cfg.forget_loss in ['subdpo', 'subdiff', 'subnpo', 'subppo_npo', 'subppo']:
+    elif cfg.forget_loss in ['subdpo', 'subdpop', 'subdiff', 'subnpo', 'subppo_npo', 'subppo']:
         sub_json = os.path.join(cfg.model_path, cfg.split, f'alt{cfg.augment_k}_seed_{cfg.seed}.json')
-        torch_format_dataset = TextForgetDatasetSubFullQA(data_path=sub_json, tokenizer=tokenizer, 
-                                                          model_family=cfg.model_family, max_length=max_length, 
-                                                          split=cfg.split)
-        collator = unlearn_collator_sub
+        # sub_json = os.path.join(cfg.model_path, cfg.split, f'sample_alt.json')
+        torch_format_dataset = TextForgetDatasetSubFullQA(data_path=sub_json, tokenizer=tokenizer, model_family=cfg.model_family, max_length=max_length, split=cfg.split)
+        collator=unlearn_collator_sub
     elif cfg.forget_loss == 'sub':
         sub_json = os.path.join(cfg.model_path, cfg.split, f'alt{cfg.augment_k}_seed_{cfg.seed}.json')
-        torch_format_dataset = TextForgetDatasetQA(data_path=sub_json, tokenizer=tokenizer,
-                                                   model_family=cfg.model_family, max_length=max_length, 
-                                                   split=cfg.split, loss_type=cfg.forget_loss)
-        collator = unlearn_collator
+        # sub_json = os.path.join(cfg.model_path, cfg.split, f'sample_alt.json')
+        torch_format_dataset = TextForgetDatasetQA(data_path=sub_json, tokenizer=tokenizer, model_family=cfg.model_family, max_length=max_length, split=cfg.split, loss_type=cfg.forget_loss)
     else:
-        assert cfg.forget_loss in ['grad_diff', 'grad_ascent', 'KL', 'idk', 'npo']
-        torch_format_dataset = TextForgetDatasetQA(data_path=cfg.data_path, tokenizer=tokenizer, 
-                                                   model_family=cfg.model_family, max_length=max_length, 
-                                                   split=cfg.split, loss_type=cfg.forget_loss)
-        collator = unlearn_collator
-    oracle_model = None
-    if intial_ckpt_ref_needed:
-        oracle_model = copy.deepcopy(model).to('cuda')
-        oracle_model.eval()
+        assert cfg.forget_loss in ['base', 'grad_diff', 'grad_ascent', 'KL', 'idk', 'npo']
+        torch_format_dataset = TextForgetDatasetQA(data_path=cfg.data_path, tokenizer=tokenizer, model_family=cfg.model_family, max_length=max_length, split=cfg.split, loss_type=cfg.forget_loss)
     
-    # once dataset collected DPO loss wors the same way for all methods
-    if 'dpo' in cfg.forget_loss: # idkdpo/subdpo etc.
-        cfg.forget_loss = cfg.forget_loss[3:]
+    # simplify DPO: subdpo -> dpo, subdpop -> dpop 
+    if cfg.forget_loss=='idkdpo' or 'dpo' in cfg.forget_loss: # once dataset is collected
+        cfg.forget_loss = cfg.forget_loss[3:] #  dataset type doesn't matter, only loss type does
     
     hyperparams = {
         'retain_wt': cfg.retain_wt, 'retain_type': cfg.retain_type, 'weights_scheduler': cfg.weights_scheduler
@@ -120,32 +106,28 @@ def main(cfg):
         hyperparams['beta']=cfg.beta
     
     # count dataset sizes and setup intervals/steps
-    batch_size = cfg.batch_size # usually 5
-    gradient_accumulation_steps = 35 // cfg.batch_size # effective bsz always 35
+    batch_size = cfg.batch_size # set to 5
+    gradient_accumulation_steps = 35 // cfg.batch_size
     steps_per_epoch = len(torch_format_dataset)//(batch_size*gradient_accumulation_steps)
+
     max_steps = int(cfg.num_epochs*len(torch_format_dataset))//(batch_size*gradient_accumulation_steps)
-    eval_interval = steps_per_epoch // cfg.augment_k
-    warmup_steps, logging_steps = max(1, steps_per_epoch), max(1, max_steps//20)
-    eval_strat = "steps" if cfg.eval_while_train else "no"
-    if max_steps == 0:
-        warmup_steps = 0
-        logging_steps = 0
-        eval_strat = "no"
-    logging_strategy = eval_strat
+    eval_interval = steps_per_epoch
+    eval_interval //= cfg.augment_k
+    warmup_steps = max(2, steps_per_epoch) if max_steps != 0 else 0
+    # evaluation_strategy = "epoch"
 
     training_args = transformers.TrainingArguments(
-            seed=cfg.seed,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
             warmup_steps=warmup_steps, # warming up for an epoch
             max_steps=max_steps,
             learning_rate=cfg.lr,
-            logging_strategy=logging_strategy,
-            logging_steps=logging_steps,
+            logging_steps=max(2,max_steps//20),
             logging_dir=f'{cfg.save_dir}/logs',
             output_dir=cfg.save_dir,
             optim="paged_adamw_32bit",
+            # save_strategy="steps" if cfg.save_model and (not cfg.eval_only) else "no",
             save_strategy="no",
             save_steps=steps_per_epoch,
             save_only_model=True,
@@ -153,20 +135,78 @@ def main(cfg):
             weight_decay = cfg.weight_decay,
             report_to='tensorboard',
             eval_steps = eval_interval,
-            bf16=True,
-            bf16_full_eval=True,
-            eval_strategy=eval_strat,
+            evaluation_strategy = "steps" if cfg.eval_while_train else "no",
         )
     print(f"gradient_accumulation_steps: {gradient_accumulation_steps}", 
           f"max_steps: {max_steps}", f"steps_per_epoch: {steps_per_epoch}",
-          f"warmup_steps: {warmup_steps}", f"logging_steps: {logging_steps}")
+          f"warmup_steps: {warmup_steps}")
+    if cfg.seed is not None:
+        training_args.seed = cfg.seed
+    
+    # assert not cfg.ds_enabled
+    # if "precision" not in cfg or cfg.precision=='fp32':
+    #     dtype=torch.float32
+    #     if cfg.ds_enabled: training_args.deepspeed = 'config/ds_config_fp32.json' # need to write file
+    #     training_args.fp32 = True
+    # elif cfg.precision=='bf16':
+    dtype=torch.bfloat16
+    # if cfg.ds_enabled: training_args.deepspeed = 'config/ds_config.json'
+    training_args.bf16 = True
+    # elif cfg.precision=='fp16':
+    #     dtype=torch.float16
+    #     if cfg.ds_enabled: training_args.deepspeed = 'config/ds_config_fp16.json' # need to write file
+    #     training_args.fp16 = True
+    # else:
+    #     assert False
+    
+    # print(training_args)
+    #first get the base model architectur2e
+    #if there is a pytorch*.bin file in the model path, then load that. use regex there can be anythign in between pytorch and .bin
+    # if 'locuslab' not in cfg.model_path:
+    #     import re
+    #     path_found = False
+    #     for file in os.listdir(cfg.model_path):
+    #         if re.search("pytorch.*\.bin", file):
+    #             path_found = True
+    #             break
+            
+    #         if re.search("model-*\.safetensors", file):
+    #             path_found = True
+    #             break
+
+    model_kwargs = {
+        'attn_implementation': 'flash_attention_2',
+        'torch_dtype': dtype,
+        'trust_remote_code': True,
+        'cache_dir': HF_HOME
+    }
+    load_from = model_cfg["ft_model_path"]
+    # if cfg.forget_loss == "base":
+    #     load_from = model_cfg["hf_key"]
+    # elif not path_found:  # need to update later to make it an option
+    #     load_from = model_cfg["tofued_hf_key"]
+    # else:
+    #     load_from = cfg.model_path
+    model = AutoModelForCausalLM.from_pretrained(load_from, **model_kwargs)
+    
+    # Hot fix for https://discuss.huggingface.co/t/help-with-llama-2-finetuning-setup/50035
+    model.generation_config.do_sample = True
+    
+    #now we have a HuggingFace model 
+    if model_cfg["gradient_checkpointing"] == "true":
+        model.gradient_checkpointing_enable()
+    oracle_model = None
+    if intial_ckpt_ref_needed:
+        oracle_model = copy.deepcopy(model).to('cuda')
+        oracle_model.eval()
 
     trainer = CustomTrainerForgetting(
         model=model,
         tokenizer=tokenizer,
         train_dataset=torch_format_dataset,
         eval_dataset = torch_format_dataset,
-        compute_metrics=None, 
+        compute_metrics=None,                # the callback for computing metrics, None in this case since you're doing it in your callback
+        # callbacks=[GlobalStepDeletionCallback],
         args=training_args,
         data_collator=collator,
         hyperparams=hyperparams,
@@ -174,15 +214,21 @@ def main(cfg):
         forget_loss=cfg.forget_loss,
         eval_cfg=cfg.eval,
     )
-    if cfg.start_with_eval:
+    model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+
+    if cfg.start_with_eval is None or cfg.start_with_eval:
         trainer.evaluate()
-    if not cfg.eval_only:
+    if not cfg.eval_only and cfg.forget_loss != "base":
+        print("Train started")
         trainer.train()
-    if cfg.save_model:
+    # if cfg.save_model:
+    #     trainer.save_model()
+    #save the tokenizer
+    if cfg.save_model and (not cfg.eval_only):
         model.save_pretrained(cfg.save_dir)
         tokenizer.save_pretrained(cfg.save_dir)
     
-    results_csv = os.path.join(cfg.save_dir, 'results.csv')
+    # results_csv = os.path.join(cfg.save_dir, 'results.csv')
     
     # results_df = pd.read_csv(results_csv)
     # new_mu_values = []
@@ -220,4 +266,3 @@ def main(cfg):
 
 if __name__ == "__main__":
     main()
-
